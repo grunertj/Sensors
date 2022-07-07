bf24961042b28976aa1e3b4dc247ba8b8b3b0224
grunertj <grunertj@gmail.com>
2022-07-07 22:20:28 +0200

    new

--- app/src/main/java/com/jwg/sensors/MainActivity.java
+++ app/src/main/java/com/jwg/sensors/MainActivity.java
@@ -2,15 +2,21 @@ package com.jwg.sensors;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,7 +30,7 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import  com.jwg.sensors.R;
+import com.jwg.sensors.R;
 
 
 import java.io.BufferedWriter;
@@ -39,13 +45,18 @@ import java.util.Locale;
 import java.util.zip.GZIPOutputStream;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener , LocationListener {
-    TextView textView,textViewX,textViewY,textViewZ,textViewT,textViewF;
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+    static final long MIN_TIME_IN_MILLISECONDS = 1000;
+    static final float MIN_DISTANCE_IN_METERS = 1;
+    TextView textView, textViewX, textViewY, textViewZ, textViewT, textViewF;
     Chronometer chronometer;
     Button buttonStart, buttonStop;
     SensorManager sensorManager;
+    LocationManager locationManager;
     Sensor accelerometer, gyroscope;
     String sensor_filename = null;
+    String currentTimeStamp = null;
+    long previous_timestamp = 0;
 
     static BufferedWriter sensor_file = null;
 
@@ -65,6 +76,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+    void location(boolean enabled) {
+        if (enabled) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.HIGH_SAMPLING_RATE_SENSORS
+                }, 1);
+            }
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, this, null);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_IN_MILLISECONDS, MIN_DISTANCE_IN_METERS, this);
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, MIN_TIME_IN_MILLISECONDS, MIN_DISTANCE_IN_METERS, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_IN_MILLISECONDS, MIN_DISTANCE_IN_METERS, this);
+        } else {
+            locationManager.removeUpdates(this);
+            locationManager = null;
+        }
+    }
+
     void sensor(boolean enabled) {
         if (enabled) {
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
@@ -75,9 +105,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    void sensor_logging(boolean enabled) {
+    void sensor_logging(boolean enabled, String currentTimeStamp) {
         if (enabled) {
-            sensor_filename = String.format("%s-%s.csv.gz", "sensor", getCurrentTimeStamp());
+            sensor_filename = String.format("%s-%s.csv.gz", "sensor", currentTimeStamp);
             try {
                 sensor_file = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(getAbsoluteFileName(sensor_filename)))));
             } catch (IOException e) {
@@ -85,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 e.printStackTrace();
             }
             try {
-                sensor_file.append("Time,Sensor,X,Y,Z\n");
+                sensor_file.append("Time,Timestamp,Sensor,X,Y,Z\n");
             } catch (IOException e) {
                 Toast.makeText(getApplicationContext(), "Could not append to file: "+ "" + e + " " + getAbsoluteFileName(sensor_filename), Toast.LENGTH_LONG).show();
                 e.printStackTrace();
@@ -120,6 +150,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         startActivity(intent);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -136,8 +167,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         chronometer = (Chronometer) findViewById(R.id.chronometer);
         textView.setText("Jens der aller Beste!");
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); // 10
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE); // 4
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.HIGH_SAMPLING_RATE_SENSORS
+            }, 1);
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         File directory = new File(directory_name);
         if (directory.exists() == false) {
@@ -149,12 +191,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                currentTimeStamp = getCurrentTimeStamp();
                 textView.setText("Start");
                 buttonStart.setEnabled(false);
                 buttonStop.setEnabled(true);
-                sensor_logging(true);
+                sensor_logging(true,currentTimeStamp);
                 sensor(true);
                 textViewF.setText(sensor_filename);
+                location(true);
                 chronometer.setBase(SystemClock.elapsedRealtime());
                 chronometer.start();
             }
@@ -166,7 +210,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 buttonStop.setEnabled(false);
                 buttonStart.setEnabled(true);
                 sensor(false);
-                sensor_logging(false);
+                sensor_logging(false,null);
+                location(false);
                 chronometer.stop();
                 openFolder();
             }
@@ -174,17 +219,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         float value[];
         long timestamp;
+        long  system_time;
         int sensor_type;
         String event_type;
 
         sensor_type = sensorEvent.sensor.getType();
 
         timestamp = sensorEvent.timestamp;
+        system_time = System.currentTimeMillis();
         value = sensorEvent.values.clone();
-        event_type = String.format(Locale.US,"%d,%d,%.8f,%.8f,%.8f\n",timestamp,sensor_type,value[0],value[1],value[2]);
+        event_type = String.format(Locale.US,"%d,%d,%d,%.8f,%.8f,%.8f\n",system_time,timestamp,sensor_type,value[0],value[1],value[2]);
         try {
             if (event_type!=null) {
                 sensor_file.append(event_type);
@@ -215,7 +280,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
@@ -223,12 +287,64 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-
+        long timestamp;
+        long  system_time;
+        float speed,latitude,longitude;
+        String event_type;
+        if ((location.hasSpeed() && location.getAccuracy() < 20 && location.hasBearing() && location.hasAccuracy())
+                || (location.hasSpeed() && location.getAccuracy() < 10 && location.hasAccuracy())) {
+            timestamp = location.getTime();
+            system_time = System.currentTimeMillis();
+            speed = (float) location.getSpeed();
+            latitude = (float) location.getLatitude();
+            longitude = (float) location.getLongitude();
+            textView.setText("TYPE_LOCATION");
+            event_type = String.format(Locale.US,"%d,%d,%d,%.8f,%.8f,%.8f\n",system_time,timestamp,100,speed,latitude,longitude);
+            try {
+                if (event_type!=null) {
+                    if (previous_timestamp != timestamp) {
+                        sensor_file.append(event_type);
+                    }
+                    previous_timestamp = timestamp;
+                }
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "Could append to file: "+ "" + e + " " + getAbsoluteFileName(sensor_filename), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void onLocationChanged(@NonNull List<Location> locations) {
         LocationListener.super.onLocationChanged(locations);
+        long timestamp;
+        long  system_time;
+        float speed,latitude,longitude;
+        String event_type;
+
+        for (Location location : locations) {
+            if ((location.hasSpeed() && location.getAccuracy() < 20 && location.hasBearing() && location.hasAccuracy())
+                    || (location.hasSpeed() && location.getAccuracy() < 10 && location.hasAccuracy())) {
+                timestamp = location.getTime();
+                system_time = System.currentTimeMillis();
+                speed = (float) location.getSpeed();
+                latitude = (float) location.getLatitude();
+                longitude = (float) location.getLongitude();
+                textView.setText("TYPE_LOCATION");
+                event_type = String.format(Locale.US,"%d,%d,%d,%.8f,%.8f,%.8f\n",system_time,timestamp,100,speed,latitude,longitude);
+                try {
+                    if (event_type!=null) {
+                        if (previous_timestamp != timestamp) {
+                            sensor_file.append(event_type);
+                        }
+                        previous_timestamp = timestamp;
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Could append to file: "+ "" + e + " " + getAbsoluteFileName(sensor_filename), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
